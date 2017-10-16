821fc279e87f65a17b3cbe932f6a85e49a21b020
Julian <julianadamwise@gmail.com>
2017-10-16 22:56:30 +0300

    re-add node site

---
+++ node_site/public/js/masonry_style.js
@@ -0,0 +1,9 @@
+// external js: masonry.pkgd.js
+
+$('.grid').masonry({
+  itemSelector: '.grid-item',
+  columnWidth: '.grid-sizer',
+  percentPosition: true,
+  columnWidth: 600,
+  fitWidth: true
+});
